c6cf452212caa0de5eb7c80380dee1964571c442
avina <avina@192.168.1.36>
2024-08-12 19:15:18 +0530

    fourth commit...

--- src/AssignmentsATB/ArraayAndScanner.java
+++ src/AssignmentsATB/ArraayAndScanner.java
@@ -13,12 +13,22 @@ try {
 		 
 		int num = s1.nextInt()	;
 		
-		System.out.println(num);
+		int num1 [] = new int [4];
+		
+		for(int i=0; i<num; i++) {
+			num1[i] = i +10;
+		}
+		
+		System.out.println("Attempting to access out of bound index");
+		System.out.println(num1[num]);
 }
+
 catch( InputMismatchException a3) {
 	System.out.println("please Enter Correct input");
 }
-
+catch(ArrayIndexOutOfBoundsException e1) {
+	System.out.println("Please Enter your input in size of the array");
+}
 
 		
 		

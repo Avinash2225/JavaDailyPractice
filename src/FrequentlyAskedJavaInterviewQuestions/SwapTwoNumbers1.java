3f06e102d7a3af1a0cdd8e5d089e4e04de8e4bec
Avinash <avinashyadav3276@gmail.com>
2026-01-21 00:01:41 +0530

    Swaptwonumber2026

---
+++ src/FrequentlyAskedJavaInterviewQuestions/SwapTwoNumbers1.java
@@ -0,0 +1,24 @@
+package FrequentlyAskedJavaInterviewQuestions;
+
+public class SwapTwoNumbers1 {
+
+	public static void main(String[] args) {
+
+		int a = 45;
+		
+		int b= 40;
+		
+	a = a + b;  // 85
+	b= a -b;
+	a = a- b; 
+	
+	System.out.println("After swapping values of.."+a+"  "+b );
+		
+		
+		
+		
+		
+	
+	}
+
+}

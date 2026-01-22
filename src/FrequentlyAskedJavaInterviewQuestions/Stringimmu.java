09e4176879473ddd835d842442084f32a86820c7
Avinash <avinashyadav3276@gmail.com>
2026-01-23 00:19:41 +0530

    string

---
+++ src/FrequentlyAskedJavaInterviewQuestions/Stringimmu.java
@@ -0,0 +1,25 @@
+package FrequentlyAskedJavaInterviewQuestions;
+
+public class Stringimmu {
+
+	public static void main(String[] args) {
+
+	/*	String a = "abc";
+		
+		String b = "avx";
+		
+		a= a.concat(b);
+		
+		System.out.println(a);  // better optimization
+		*/
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(false);  // 16 capacity ki array create ho jati hai
+		
+		
+		
+		
+	}
+
+}

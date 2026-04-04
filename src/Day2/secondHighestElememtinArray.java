6a678886991162e9cbe06410ee481a909087d8dc
Avinash <avinashyadav3276@gmail.com>
2026-04-05 00:12:47 +0530

    Secondhighestelement

---
+++ src/Day2/secondHighestElememtinArray.java
@@ -0,0 +1,25 @@
+package Day2;
+
+public class secondHighestElememtinArray {
+
+	public static void main(String[] args) {
+
+		int arr[] = {10,20,30,40,50,90};
+		
+		int max1 = arr[0];
+		int max2 = arr[1];
+		
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]>max1) {
+				max2 = max1;
+				max1= arr[i];
+				
+			}else if(arr[i]>max2 && arr[i]!= max1 ) {
+			}
+		
+		}
+		System.out.println("second highest element of array is"+ max2);	
+
+	}
+
+}
